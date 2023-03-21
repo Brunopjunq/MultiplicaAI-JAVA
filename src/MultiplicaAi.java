f2be140db2eb77470644696b6e31b03915e7e2e3
Brunopjunq <bruno_junq@poli.ufrj.br>
2023-03-21 12:15:57 -0300

    fix: close sc

--- src/MultiplicaAi.java
+++ src/MultiplicaAi.java
@@ -5,6 +5,7 @@ public class MultiplicaAi {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         final int number = sc.nextInt();
+        sc.close();
         System.out.println("Tabela de multiplicação de " + number);
         for (int i = 1; i <= 10; i++) {
             System.out.println(number + " x " + i + " = " +  number * i);
